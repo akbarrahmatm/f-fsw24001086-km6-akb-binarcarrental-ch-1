71ed6716440db202c65e75728d556636b3778e6f
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-02-22 17:06:22 +0700

    section testimonial done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel();
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  autoWidth: true,
+  margin: 20,
+  nav: true,
+  dots: false,
+  center: true,
+  navText: [
+    "<img class='icon-control-carousel' src='./assets/img/svg/left-button.svg' alt='Previous'>",
+    "<img class='icon-control-carousel' src='./assets/img/svg/right-button.svg' alt='Next'>",
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
 });
