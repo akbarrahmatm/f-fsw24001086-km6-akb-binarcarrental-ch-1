23760700d755bbbe22eb9eb7d56ca00edfd0ea48
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-02-26 13:01:33 +0700

    fix responsive on testimonial, why us card, our services

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,55 @@
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  autoWidth: true,
-  margin: 20,
-  nav: true,
-  dots: false,
-  center: true,
-  navText: [
-    "<img class='icon-control-carousel' src='./assets/img/svg/left-button.svg' alt='Previous'>",
-    "<img class='icon-control-carousel' src='./assets/img/svg/right-button.svg' alt='Next'>",
-  ],
-  responsive: {
-    0: {
-      items: 1,
+// $(".owl-carousel").owlCarousel({
+//   loop: true,
+//   autoWidth: true,
+//   margin: 20,
+//   nav: true,
+//   dots: false,
+//   center: true,
+//   navText: [
+//     "<img class='icon-control-carousel' src='./assets/img/svg/left-button.svg' alt='Previous'>",
+//     "<img class='icon-control-carousel' src='./assets/img/svg/right-button.svg' alt='Next'>",
+//   ],
+//   responsiveClass: true,
+//   responsive: {
+//     0: {
+//       items: 1,
+//       nav: true,
+//     },
+//     600: {
+//       items: 3,
+//       nav: true,
+//     },
+//     1000: {
+//       items: 5,
+//       nav: true,
+//     },
+//   },
+// });
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    dots: false,
+    navText: [
+      "<img class='icon-control-carousel' src='./assets/img/svg/left-button.svg' alt='Previous'>",
+      "<img class='icon-control-carousel' src='./assets/img/svg/right-button.svg' alt='Next'>",
+    ],
+    responsiveClass: true,
+
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      900: {
+        items: 2,
+        nav: true,
+      },
+      1300: {
+        items: 3,
+        nav: true,
+      },
     },
-    600: {
-      items: 3,
-    },
-    1000: {
-      items: 5,
-    },
-  },
+  });
 });
