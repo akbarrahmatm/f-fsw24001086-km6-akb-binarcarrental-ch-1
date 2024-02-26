34417c785b06d719bbd3cbd7f2a5c540dcb5deb0
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-02-26 13:40:00 +0700

    fix responsive of hero car image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,3 @@
-// $(".owl-carousel").owlCarousel({
-//   loop: true,
-//   autoWidth: true,
-//   margin: 20,
-//   nav: true,
-//   dots: false,
-//   center: true,
-//   navText: [
-//     "<img class='icon-control-carousel' src='./assets/img/svg/left-button.svg' alt='Previous'>",
-//     "<img class='icon-control-carousel' src='./assets/img/svg/right-button.svg' alt='Next'>",
-//   ],
-//   responsiveClass: true,
-//   responsive: {
-//     0: {
-//       items: 1,
-//       nav: true,
-//     },
-//     600: {
-//       items: 3,
-//       nav: true,
-//     },
-//     1000: {
-//       items: 5,
-//       nav: true,
-//     },
-//   },
-// });
-
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
